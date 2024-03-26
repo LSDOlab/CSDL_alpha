1bd3b2bf0da35f36b9a813497a1fa03da05b07d2
msperryucsd <msperry@ucsd.edu>
2024-03-25 18:53:20 -0700

    added operation to graph, added ad_edge, and simple add operation

---
+++ examples/basic_examples/example_add.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+    import csdl_alpha as csdl
+    import numpy as np
+
+
+    import time as time
+    # import cProfile
+    # profiler = cProfile.Profile()
+    # profiler.enable()
+
+    start = time.time()
+    recorder = csdl.build_new_recorder(inline = True)
+    recorder.start()
+    x = csdl.Variable((1,), name = 'x', value = np.ones((1,))*3.0)
+    z = csdl.Variable((1,), name = 'z', value = np.ones((1,))*2.0)
+
+    for i in range(5):
+        z = csdl.add(x,z)
+        z.name = f'z_{i}'
+
+    print(z.value)
+
+    # recorder.active_graph.visualize()
+    recorder.stop()
+    # profiler.disable()
+    # profiler.dump_stats('output')
+
+    end = time.time()
+    print(f'Time: {end - start}')
