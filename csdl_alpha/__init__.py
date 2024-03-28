b5d52e84ec1c3f2f6254ab975d9ebc433cea8ff6
Anugrah Jo Joshy <44123535+anugrahjo@users.noreply.github.com>
2024-03-28 15:17:53 -0700

    minor changes to operation imports

--- csdl_alpha/__init__.py
+++ csdl_alpha/__init__.py
@@ -3,8 +3,6 @@ __version__ = '0.0.0'
 
 # from .manager import get_current_recorder, print_all_recorders, build_new_recorder
 
-from csdl_alpha.src.operations.add import add
-from csdl_alpha.src.operations.mult import mult
-from csdl_alpha.src.operations.square import square
+from csdl_alpha.src.operations import *
 
 from .api import *
---
+++ csdl_alpha/src/operations/__init__.py
@@ -0,0 +1,12 @@
+# Basic operations
+from .add import add
+from .mult import mult
+from .square import square
+
+# Trigonometric operations
+
+
+# Linear algebra operations
+
+
+# Tensor operations
